7dd28aebe4ffeb977578e9dd910ebb7b79a52a09
htmlucas <lucasmartinsde@gmail.com>
2024-06-12 18:00:56 -0300

    First teste

---
+++ src/services/clubs-services.spec.ts
@@ -0,0 +1,9 @@
+describe('Testando alguma coisa',() => {
+
+    it('Numero ser 1',() => {
+        const number = 1;
+        //expect(number).not.toBe(1)
+        expect(number).toBe(1)
+    });
+    
+})
